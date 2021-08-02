6a6f05012a206bf2def30ca493ea1af0c5656e0f
NikitaMatckevich <nikitamatskevitch@gmail.com>
2021-08-02 16:08:52 +0000

    rewrite tests

--- src/main.c
+++ src/main.c
@@ -19,7 +19,7 @@ main()
 	}
 
 	/* Initialize SSD NVMe controller */
-	struct ctrlr_entry *ctrlr = ctrlr_entry_init(&opts, 9);
+	struct ctrlr_entry *ctrlr = ctrlr_entry_init(&opts, 19);
 	struct ns_entry *ns = TAILQ_FIRST(&ctrlr->ns);
 
 	uint32_t block_number = MAX_CHUNK_CACHESIZE;
@@ -35,22 +35,29 @@ main()
 	struct spdk_nvme_qpair *qpair = spdk_nvme_ctrlr_alloc_io_qpair(ctrlr->ctrlr, NULL, 0);
 	
 	/* Initialize a chunk */
-	uint64_t capacity = 1 << 20;
+	uint64_t capacity = 1 << 27;
 
 	ssd_chunk *chunk = ssd_chunk_init(ctrlr, qpair, cache, sizeof(double) * capacity);
 	
-	/* Dump chunk info */
 	ssd_chunk_print(chunk);
 
-	{
+	double avg = 0;
+	for (int num_iter = 0; num_iter < 10; num_iter++) {
 		uint64_t beg = spdk_get_ticks();
-		// Ordered write
 		for (uint64_t i = 0; i < capacity; i++) {
 			ssd_chunk_write_double(chunk, i, 0.1 * i);
 		}
+		ssd_chunk_sync(chunk);
 		uint64_t end = spdk_get_ticks();
-		printf("test ord. write: 1 GiB / %5.3e ms\n", 1.0 * (end - beg) / spdk_get_ticks_hz() * 1000);
+		
+		double duration = 1.0 * (end - beg) / spdk_get_ticks_hz() * 100;
+		printf("test ord. write #%d, speed = 1 GiB / %5.5e ms\n", num_iter, duration * 10);
+	
+		avg += duration;
 	}
+	printf("test ord. write: average speed = 1 GiB / %5.3e ms\n", avg);
+
+	ssd_chunk_print(chunk);
 
 	/* Free a chunk */
 	ssd_chunk_free(chunk);
