441c3efa93bba20e5e9325a2c37567a7bca924eb
NikitaMatckevich <nikitamatskevitch@gmail.com>
2021-08-09 08:28:18 +0000

    warmup iterations

--- src/main.c
+++ src/main.c
@@ -37,13 +37,17 @@ main()
 	/* Initialize a chunk */
 	uint64_t capacity = 1 << 27;
 
+	/* Setup */
 	ssd_chunk *chunk = ssd_chunk_init(ctrlr, qpair, cache, sizeof(double) * capacity);
 	
-	ssd_chunk_print(chunk);
+	for (uint64_t i = 0; i < capacity; i++) {
+		ssd_chunk_write_double(chunk, i, 0.);
+	}
 
 	double avg = 0;
 	double rec;
 
+	/* Tests */
 	for (int num_iter = 0; num_iter < 10; num_iter++) {
 		uint64_t beg = spdk_get_ticks();
 		for (uint64_t i = 0; i < capacity; i++) {
@@ -54,12 +58,14 @@ main()
 		
 		double duration = 1.0 * (end - beg) / spdk_get_ticks_hz() * 100;
 		printf("test ord. write #%d, speed = 1 GiB / %5.5e ms\n", num_iter, duration * 10);
-	
-		avg += duration;
+		
+		if (num_iter >= 5) {
+			avg += duration * 2;
+		}
 	}
 	printf("test ord. write: average speed = 1 GiB / %5.3e ms\n", avg);
 
-	ssd_chunk_print(chunk);
+	//ssd_chunk_print(chunk);
 
 	/* Free a chunk */
 	ssd_chunk_free(chunk);
